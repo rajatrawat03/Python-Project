4d21fef2328563aa9cff636a43a0418f380463fe
rajatrawat03 <113665955+rajatrawat03@users.noreply.github.com>
2024-10-04 08:37:36 +0530

    Add files via upload

--- database.sql
+++ database.sql
@@ -4,4 +4,4 @@ LIMIT 0, 5000
 
 -- Date: 2024-10-01 12:20
 */
-INSERT INTO `` (`Id`,`Name`,`Email_id`,`Phoneno`,`Address`,`Post`,`Salary`) VALUES (1,'Akash Modanwal','akash@gmail.com',9026056760,'Greater Noida','Data Analyst',80000);
+INSERT INTO `` (`Id`,`Name`,`Email_id`,`Phoneno`,`Address`,`Post`,`Salary`) VALUES (1,'Rajat','rajat0312@gmail.com',9988776655,'Noida','Data Analyst',80000);
